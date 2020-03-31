2c876a76832511b4e24db572f3c602c0468e15c7
h32109 <h32109@gmail.com>
2020-03-31 21:31:08 +0900

    commit
    commit

---
+++ factorial/factorial/factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+unsigned int f, x;
+unsigned int factorial(unsigned int a);
+void main() {
+	puts("Enter an integer value between 1 and 8 : ");
+	scanf("%d", &x);
+	if (x > 8 || x < 1) {
+		printf("Only values from 1 to 8 are aceeptable!");
+	}
+	else {
+		f = factorial(x);
+		printf("%u factoral equals %u \n", x, f);
+	}
+}
+unsigned int factorial(unsigned int a) {
+	if (a == 1)
+		return 1;
+	else {
+		a *= factoral(a - 1);
+		return a;
+	}
+}
